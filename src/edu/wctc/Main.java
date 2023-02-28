f6fc42dd1aa10180c6927158afe00654f3d6af18
JackHoward <jhoward000@live.com>
2023-02-28 16:01:27 -0600

    Bug fixes

--- src/edu/wctc/Main.java
+++ src/edu/wctc/Main.java
@@ -20,20 +20,22 @@ public class Main {
                     yearsApart.getMonths(),
                     yearsApart.getDays());
 
+            System.out.println();
+
             //find out time between albums
             Period betweenAlbums = Period.between(joshuaTree, rattleAndHum);
             System.out.printf("The albums are %d years, %d months, and %d days apart.\n",
-                    yearsApart.getYears(),
-                    yearsApart.getMonths(),
-                    yearsApart.getDays());
+                    betweenAlbums.getYears(),
+                    betweenAlbums.getMonths(),
+                    betweenAlbums.getDays());
 
-
+            System.out.println();
             //calculate when third album should have been
 
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM dd, yyyy");
 
             LocalDate nextChild = rattleAndHum.plus(betweenAlbums);
-            System.out.printf("Next album should have been released on %s\n",
+            System.out.printf("The next album should have been released on %s.\n",
                     nextChild.format(formatter));
 
         }catch (DateTimeException e){
